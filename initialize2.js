214b636254ced5f596e48389ebaad3155daa1372
ajk121 <contact.abdulkhan@gmail.com>
2015-04-21 13:50:40 +0100

    adding services.html

---
+++ initialize2.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+      $('#fullpage2').fullpage({
+        sectionsColor: ['#fff', 'black', '#fff', 'black'],
+        anchors: ['7thPage', '8thPage', '9thPage', '10thPage'],
+        menu: '#menu',
+        scrollingSpeed: 1500,
+        verticalCentered: false,
+        autoScrolling: false,
+        fitToSection: false
+      });
+
+    });
