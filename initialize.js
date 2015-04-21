760624ea03873ba2cbe661a887fd8fe61c73a8f1
ajk121 <contact.abdulkhan@gmail.com>
2015-04-21 01:12:34 +0100

    refactoring

--- initialize.js
+++ initialize.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
-    $('#fullpage').fullpage();
-});
+      $('#fullpage').fullpage({
+        sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
+        anchors: ['firstPage', 'secondPage', '3rdPage', '4thPage', '5thPage', '6thPage'],
+        menu: '#menu',
+        scrollingSpeed: 1500,
+        verticalCentered: false,
+        autoScrolling: false,
+        fitToSection: false
+      });
+
+    });
