b3fcfc2661d4a1350e563f08a964a1cdbe4b2b73
ajk121 <contact.abdulkhan@gmail.com>
2015-04-25 14:07:44 +0100

    fixing media queries

--- initialize.js
+++ initialize.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
       $('#fullpage').fullpage({
-        sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
+        // sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
         anchors: ['firstPage', 'secondPage', '3rdPage', '4thPage', '5thPage', '6thPage'],
         menu: '#menu',
         scrollingSpeed: 1500,
